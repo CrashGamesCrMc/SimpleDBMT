117c758846fb292d5603f109dd059a5e2260d963
CrashGamesCrMc <crash.games.cr.mc.yt@gmail.com>
2019-10-21 15:34:22 +0200

    Adapter Info for external use

---
+++ src/io/github/SebastianDanielFrenz/SimpleDBMT/adapter/AdapterInfo.java
@@ -0,0 +1,9 @@
+package io.github.SebastianDanielFrenz.SimpleDBMT.adapter;
+
+import io.github.SebastianDanielFrenz.SimpleDBMT.varTypes.Version;
+
+public class AdapterInfo {
+
+	public static final Version version = new Version(new int[] { 2, 0, 0, 0 });
+
+}
